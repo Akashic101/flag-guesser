eafbc9df05a85f43dda782e9bb4afc27c1a1aa69
David Moll <david.leander.moll@gmail.com>
2023-08-13 22:23:00 +0200

    Added 1-minute-game, improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,15 @@ import {
   Center,
   Space,
   MultiSelect,
+  Modal,
+  Title,
 } from "@mantine/core";
-import { useColorScheme, useLocalStorage } from "@mantine/hooks";
+import { useColorScheme, useDisclosure, useLocalStorage } from "@mantine/hooks";
 import { SegmentedToggle } from "./components/segmentedToggle";
 import ReactCountryFlag from "react-country-flag";
 import { useEffect, useState } from "react";
 import Logo from "./components/logo";
+import { ModalsProvider } from "@mantine/modals";
 
 interface Country {
   countryFullName: string;
@@ -54,12 +57,17 @@ export default function App() {
     getInitialValueInEffect: true,
   });
 
+  const [opened, { open, close }] = useDisclosure(false);
+
   const [correctChoices, setCorrectChoices] = useState(0);
   const [wrongChoices, setWrongChoices] = useState(0);
   const [correctCountryInfo, setCorrectCountryInfo] = useState("");
   const [continentFilters, setContinentFilters] = useState<ContinentFilters>(
     initialContinentFilters
   );
+
+  const [timeLeft, setTimeLeft] = useState<number>(60); // 60 seconds
+  const [gameStarted, setGameStarted] = useState<boolean>(false); // Track if the game has started
 
   const [selectedCountries, setSelectedCountries] = useState<Country[]>([]);
   const [randomCountry, setRandomCountry] = useState<Country>();
@@ -71,50 +79,21 @@ export default function App() {
     (country) => continentFilters[country.continent]
   );
 
-  const getRandomCountryWithDistance = (lastIndex: number | null) => {
-    let newIndex;
-    do {
-      newIndex = Math.floor(Math.random() * filteredCountries.length);
-    } while (lastIndex !== null && Math.abs(newIndex - lastIndex) <= 20);
-    return newIndex;
-  };
-
   const getRandomCountries = () => {
-    const lastRandomIndex = randomCountry
-      ? filteredCountries.indexOf(randomCountry)
-      : null;
-    const randomIndex = getRandomCountryWithDistance(lastRandomIndex);
+    const shuffledCountries = filteredCountries.sort(() => 0.5 - Math.random());
+    const selected = shuffledCountries.slice(0, 4);
+    setSelectedCountries(selected);
 
-    const selected = [randomIndex];
-    let count = 1;
-
-    while (count < 4) {
-      const newIndex = getRandomCountryWithDistance(
-        selected[selected.length - 1]
-      );
-      selected.push(newIndex);
-      count++;
-    }
-
-    const selectedCountriesArray = selected.map(
-      (index) => filteredCountries[index]
-    );
-    setSelectedCountries(selectedCountriesArray);
-
-    setRandomCountry(selectedCountriesArray[0]);
+    const randomIndex = Math.floor(Math.random() * selected.length);
+    setRandomCountry(selected[randomIndex]);
     setCorrectCountryInfo("");
   };
 
-  const compareCountry = (
-    selectedCountry:
-      | { countryFullName: String; countryISOCode: String }
-      | null
-      | undefined
-  ) => {
+  const compareCountry = (selectedCountry: Country) => {
     if (selectedCountry === randomCountry) {
       setCorrectChoices(correctChoices + 1);
       getRandomCountries();
-      setCorrectCountryInfo("");
+      setCorrectCountryInfo("That was the correct answer");
     } else {
       setWrongChoices(wrongChoices + 1);
       getRandomCountries();
@@ -147,6 +126,21 @@ export default function App() {
     getRandomCountries();
   }, []);
 
+  useEffect(() => {
+    if (gameStarted && timeLeft === 0) {
+      open();
+      setGameStarted(false);
+    }
+
+    if (gameStarted) {
+      const interval = setInterval(() => {
+        setTimeLeft((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [timeLeft, correctChoices, gameStarted]);
+
   return (
     <ColorSchemeProvider
       colorScheme={colorScheme}
@@ -157,81 +151,116 @@ export default function App() {
         withGlobalStyles
         withNormalizeCSS
       >
-        <AppShell
-          padding="md"
-          header={
-            <Header height={60}>
-              {
-                <Group sx={{ height: "100%" }} px={10} position="apart">
-                  <Group sx={{ height: "100%" }}>
+        <ModalsProvider>
+          <AppShell
+            padding="md"
+            header={
+              <Header height={60}>
+                {
+                  <Group sx={{ height: "100%" }} px={10} position="apart">
                     <Logo />
-                  </Group>
-                  <Group>
                     <SegmentedToggle />
                   </Group>
-                </Group>
-              }
-            </Header>
-          }
-        >
-          <MultiSelect
-            data={continents.map((continent) => ({
-              value: continent,
-              label: continent,
-            }))}
-            value={Object.keys(continentFilters).filter(
-              (continent) => continentFilters[continent]
-            )}
-            onChange={handleMultiSelectChange}
-            size="xl"
-            placeholder="Select continents"
-            multiple
-          />
-          <Space h={"md"} />
-          <Stack align="end">
-            <Button
-              variant="gradient"
-              gradient={{ from: "orange", to: "red" }}
-              onClick={resetCounters}
-            >
-              Reset Counters
-            </Button>
-          </Stack>
-          <Center maw={100} h={300} mx="auto">
-            {randomCountry ? (
-              <div
-                style={{
-                  boxShadow: "1px 2px 9px #000000",
-                }}
-              >
-                <ReactCountryFlag
-                  countryCode={randomCountry.countryISOCode.toString()}
-                  svg
-                  style={{
-                    width: "20em",
-                    height: "15em",
+                }
+              </Header>
+            }
+          >
+            <MultiSelect
+              data={continents.map((continent) => ({
+                value: continent,
+                label: continent,
+              }))}
+              value={Object.keys(continentFilters).filter(
+                (continent) => continentFilters[continent]
+              )}
+              onChange={handleMultiSelectChange}
+              size="xl"
+              placeholder="Select continents"
+              multiple
+            />
+            <Space h={"md"} />
+            <Stack align="end">
+              {!gameStarted && (
+                <Button
+                  variant="gradient"
+                  gradient={{ from: "orange", to: "red" }}
+                  onClick={resetCounters}
+                >
+                  Reset Counters
+                </Button>
+              )}
+
+              {!gameStarted && (
+                <Button
+                  color="blue"
+                  onClick={() => {
+                    setTimeLeft(60);
+                    setCorrectChoices(0);
+                    setWrongChoices(0);
+                    setGameStarted(true); // Start the game when the button is clicked
+                    getRandomCountries();
                   }}
-                />
-              </div>
-            ) : (
-              <Text></Text>
-            )}
-          </Center>
-          <Stack align="center">
-            {selectedCountries.map((country) => (
-              <Button
-                w={350}
-                size={"xl"}
-                onClick={() => compareCountry(country)}
-              >
-                {country.countryFullName}
-              </Button>
-            ))}
-            {correctCountryInfo && <Text fz="xl">{correctCountryInfo}</Text>}
-            <Text fz="xl">Correct Choices: {correctChoices}</Text>
-            <Text fz="xl">Wrong Choices: {wrongChoices}</Text>
-          </Stack>
-        </AppShell>
+                >
+                  Start 1 Minute game
+                </Button>
+              )}
+              {gameStarted && (
+                <Text fw={700}>Time left: {timeLeft} seconds</Text>
+              )}
+            </Stack>
+            <Center maw={100} h={300} mx="auto">
+              {randomCountry ? (
+                <div
+                  style={{
+                    boxShadow: "1px 2px 9px #000000",
+                  }}
+                >
+                  <ReactCountryFlag
+                    countryCode={randomCountry.countryISOCode.toString()}
+                    svg
+                    style={{
+                      width: "20em",
+                      height: "15em",
+                    }}
+                  />
+                </div>
+              ) : (
+                <Text>A country should be placed here</Text>
+              )}
+            </Center>
+            <Stack align="center">
+              {selectedCountries.map((country) => (
+                <Button
+                  w={350}
+                  size={"xl"}
+                  onClick={() => compareCountry(country)}
+                >
+                  {country.countryFullName}
+                </Button>
+              ))}
+              {correctCountryInfo && <Text fz="xl">{correctCountryInfo}</Text>}
+              <Text c="green" fz="xl" fw={500}>
+                Correct Choices: {correctChoices}
+              </Text>
+              <Text c="red" fz="xl" fw={500}>
+                Wrong Choices: {wrongChoices}
+              </Text>
+            </Stack>
+          </AppShell>
+          <Modal id="deleteAusschreibungModal" opened={opened} onClose={close}>
+            <Title variant="gradient" gradient={{ from: "green", to: "blue" }}>
+              Game over
+            </Title>
+            <Text id="modalText" c="dimmed">
+              You got {correctChoices} out of {correctChoices + wrongChoices}.
+              That is{" "}
+              {Math.round(
+                (correctChoices / (correctChoices + wrongChoices)) * 100
+              )}
+              %
+            </Text>
+          </Modal>
+        </ModalsProvider>
       </MantineProvider>
     </ColorSchemeProvider>
   );
